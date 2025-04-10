6d39c0f833a75d22199b98c0778d820c311a112b
Marlon Rib <119014294+marlonrt@users.noreply.github.com>
2025-04-10 00:00:12 -0300

    Add files via upload
    Update, new game (Xandaum)

--- translations.js
+++ translations.js
@@ -1,11 +1,8 @@
 const translations = {
     en: {
         quote: "A journey of a thousand miles begins with a single step.",
-<<<<<<< HEAD
         "xandaum-title": "Xandaum",
         "xandaum-desc": "Do you have what it takes to rule with the pen of justice?",
-=======
->>>>>>> origin/main
         "natal-title": "Natal Perdido",
         "natal-desc": "A Brazilian indie RPG with a biblical perspective.",
         "simulove-title": "SimuLove! +18",
@@ -14,19 +11,13 @@ const translations = {
         "vida-desc": "As an advanced AI.",
         "colors-title": "Colors of the Past",
         "colors-desc": "A drama about a pianist who stopped playing after a traumatic event.",
-<<<<<<< HEAD
         "trailer": "Trailer",
-=======
->>>>>>> origin/main
         footer: "© 2025 YEON Studio. All rights reserved."
     },
     pt: {
         quote: "Uma jornada de mil milhas começa com um único passo.",
-<<<<<<< HEAD
         "xandaum-title": "Xandaum",
         "xandaum-desc": "Você tem o que é preciso para governar com a caneta da justiça?",
-=======
->>>>>>> origin/main
         "natal-title": "Natal Perdido",
         "natal-desc": "Um RPG indie brasileiro com uma perspectiva bíblica.",
         "simulove-title": "SimuLove! +18",
@@ -35,19 +26,13 @@ const translations = {
         "vida-desc": "Como uma IA avançada.",
         "colors-title": "Colors of the Past",
         "colors-desc": "Um drama sobre uma pianista que parou de tocar após um evento traumático.",
-<<<<<<< HEAD
         "trailer": "Trailer",
-=======
->>>>>>> origin/main
         footer: "© 2025 YEON Studio. Todos os direitos reservados."
     },
     kr: {
         quote: "천리 길도 한 걸음부터.",
-<<<<<<< HEAD
         "xandaum-title": "샨다움",
         "xandaum-desc": "정의의 펜으로 통치할 준비가 되셨나요?",
-=======
->>>>>>> origin/main
         "natal-title": "잃어버린 크리스마스",
         "natal-desc": "성경적 관점을 가진 브라질 인디 RPG.",
         "simulove-title": "SimuLove! +18",
@@ -56,10 +41,7 @@ const translations = {
         "vida-desc": "고급 AI로서.",
         "colors-title": "과거의 색깔",
         "colors-desc": "트라우마 이후 연주를 멈춘 피아니스트에 대한 드라마.",
-<<<<<<< HEAD
         "trailer": "트레일러",
-=======
->>>>>>> origin/main
         footer: "© 2025 YEON Studio. 모든 권리 보유."
     }
 };
@@ -76,4 +58,9 @@ document.querySelectorAll(".language-selector").forEach(button => {
         const language = button.getAttribute("data-language");
         setLanguage(language);
     });
+});
+
+// Definir o idioma padrão
+document.addEventListener("DOMContentLoaded", function() {
+    setLanguage("en");
 });
